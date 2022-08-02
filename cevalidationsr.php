fff1016d1909caa6a677f2e0328fdbf661cff580
Kumar Agarwal <kagarwal@ucsc.edu>
2022-08-02 09:23:23 -0700

    bump version

--- cevalidationsr.php
+++ cevalidationsr.php
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('CEVALIDATIONSR_VERSION', '1.0.0');
+define('CEVALIDATIONSR_VERSION', '1.0.3');
 
 /**
  * The code that runs during plugin activation.
